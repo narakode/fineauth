25ce9c525c5078948e21b6fb282ccd66ca2b6d46
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-07-03 17:25:03 +0700

    add timestamp to migration

--- src/FineAuthServiceProvider.php
+++ src/FineAuthServiceProvider.php
@@ -10,7 +10,7 @@ class FineAuthServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishesMigrations([
-                __DIR__ . '/../database/migrations' => database_path('migrations')
+                __DIR__ . '/../database/migrations/create_refresh_tokens_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_refresh_tokens_table.php')
             ]);
         }
     }
